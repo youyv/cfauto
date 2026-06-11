0369b351ebb9241ccf9cdcf7d11d0809e64027ba
youyv <youyv@users.noreply.github.com>
2026-06-11 19:25:14 +0800

    chore: bump to V10.10.1

--- src/index.ts
+++ src/index.ts
@@ -97,7 +97,7 @@ function mainHtml() {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="manifest" href="/manifest.json">
-    <title>Worker 智能中控 (V10.10.0)</title>
+    <title>Worker 智能中控 (V10.10.1)</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <style>
